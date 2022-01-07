26484cd8695ba24f7f0965053e267ca975a04bc9
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-07 14:50:33 +0300

    python delete commit

---
+++ delete.py
@@ -0,0 +1,35 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    database="testdb",
+    user="postgres",
+    password="@S3ns31;",
+    host="127.0.0.1",
+    port=5432
+)
+print("Opened database successfully!")
+
+cur = conn.cursor()
+
+cur.execute(
+    '''
+    DELETE FROM python_company WHERE id = 5;
+    '''
+)
+conn.commit()
+print('Deleted {} rows\n'.format(cur.rowcount))
+
+cur.execute(
+    '''
+    SELECT * FROM python_company;
+    '''
+)
+rows = cur.fetchall()
+for row in rows:
+    print("ID = ", row[0])
+    print("NAME = ", row[1])
+    print("ADDRESS = ", row[2])
+    print("SALARY = ", row[3])
+    print('\n')
+print('Operation done successfully!')
+conn.close()
